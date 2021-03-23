d6f4aca4b95da3a9e23e9ff90c7c595fb1419890
Nico Mannucci <nmannucci1@gmail.com>
2021-03-23 00:12:37 -0700

    deleted focus state

--- src/components/UI/Input.svelte
+++ src/components/UI/Input.svelte
@@ -4,5 +4,5 @@
 
 <input
   {type}
-  class="px-12px py-1 mt-8px w-full text-lg border border-hex-6E84A3 rounded-md shadow-sm focus:ring-2 focus:border-indigo-900"
+  class="px-12px py-1 mt-8px w-full text-lg border border-hex-6E84A3 rounded-md shadow-sm"
 />
